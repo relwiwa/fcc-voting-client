489f0a547e49a75dca8b1a866031354d354f1ecd
Walter <relw1wa-github@yahoo.de>
2018-09-09 21:32:43 +0100

    Update Dashboard component to use  BasenameContext for links

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -4,26 +4,29 @@ import React from 'react';
 
 import IconLink from '../../../reusable-components/icon-link';
 import PollsList from './PollsList';
+import BasenameContext from '../config/BasenameContext';
 
 import './Dashboard.scss';
 
 const Dashboard = ({ error, latestPolls, popularPolls }) => {
   return <div className="dashboard">
-    <div className="grid-x grid-padding-x grid-padding-y grid-margin-x text-center">
-      <h3 className="cell subheader">Collaborative decision making has finally arrived with Decisions, Decisions</h3>
-      <div className="cell medium-4 callout primary">
-        <p><FontAwesomeIcon icon="sign-in-alt" size="2x" /></p>
-        <p><IconLink link="/sign-in" icon="sign-in-alt" text="Sign in" /> now and let people around the globe help you in making decisions by creating your own polls.</p>
-      </div>
-      <div className="cell medium-4 callout primary">
-        <p><FontAwesomeIcon icon="user-plus" size="2x" /></p>
-        <p>If you do not have an account yet, now is the time to <IconLink link="/sign-up" icon="user-plus" text="sign up" />.</p>
-      </div>
-      <div className="cell medium-4 callout primary">
-        <p><FontAwesomeIcon icon="times-circle" size="2x" /></p>
-        <p>Or, check out other people's polls and <IconLink link="/frontend-projects/voting/polls" icon="times-circle" text="cast your vote" /></p>
-      </div>
-    </div>
+    <BasenameContext.Consumer>
+      {basename => <div className="grid-x grid-padding-x grid-padding-y grid-margin-x text-center">
+        <h3 className="cell subheader">Collaborative decision making has finally arrived with Decisions, Decisions</h3>
+        <div className="cell medium-4 callout primary">
+          <p><FontAwesomeIcon icon="sign-in-alt" size="2x" /></p>
+          <p><IconLink link="/sign-in" icon="sign-in-alt" text="Sign in" /> now and let people around the globe help you in making decisions by creating your own polls.</p>
+        </div>
+        <div className="cell medium-4 callout primary">
+          <p><FontAwesomeIcon icon="user-plus" size="2x" /></p>
+          <p>If you do not have an account yet, now is the time to <IconLink link="/sign-up" icon="user-plus" text="sign up" />.</p>
+        </div>
+        <div className="cell medium-4 callout primary">
+          <p><FontAwesomeIcon icon="times-circle" size="2x" /></p>
+          <p>Or, check out other people's polls and <IconLink link={`${basename}polls`} icon="times-circle" text="cast your vote" /></p>
+        </div>
+      </div>}
+    </BasenameContext.Consumer>
     <hr />
     {error && <div className="grid-x">
       <div className="cell callout alert">{error}</div>
